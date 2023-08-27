44691ffc516429b8186b50a987f0cbfa781473a7
SantiagoVargas <santiago19267@gmail.com>
2023-08-26 21:34:29 -0500

    Ejercicio inicial en PYTHON

---
+++ ejePrueba.py
@@ -0,0 +1,30 @@
+import random
+
+if "ARTA"<"M":
+    print("SI")
+else:
+    print("NO")
+    
+#ejemplo listas
+
+ciudades=[]
+frutas=["manzana","pera"]
+wa=[23,"wa",False,2.22]
+print(wa[3])
+
+for f in frutas:
+    print(frutas)
+    
+for x in range(3):
+    ciudad=input("ingrese ciudad: ")
+    ciudades.append(ciudad)
+    
+print(ciudades)
+
+numeros=[23,3,2]
+menor=min(numeros)
+print(menor)
+
+aleatorio= random.random()
+print(aleatorio)
+
